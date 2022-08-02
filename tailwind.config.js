f254c1609cb8e041b63573413bd15336d6bfa548
CTILET <ctilet757@gmail.com>
2022-08-02 22:09:12 +0500

    Youtube clone started

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: [
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      colors: {
+        gray: "#F0F1F2",
+      },
+      spacing: {
+        "8xl": "96rem",
+        "9xl": "128rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
+  },
+ 
+}
